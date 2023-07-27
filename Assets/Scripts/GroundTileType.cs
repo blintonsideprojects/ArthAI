ec4ec139013bd77c953d7843678cd9f4833bd4d7
Brian Linton <blintonsideprojects@gmail.com>
2023-07-26 17:44:37 -0700

    initial terrain generation

---
+++ Assets/Scripts/GroundTileType.cs
@@ -0,0 +1,11 @@
+    public enum GroundTileType
+    {
+        /* 0 - 1000 */
+        DeepWater = 0,
+        UndeepWater = 1,
+        Beach = 2,
+        Grass = 3,
+        Dirt = 4,
+        Mountain = 5,
+        Snow = 6
+    }
